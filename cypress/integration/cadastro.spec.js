a38e4e78bad91bbba72506c30030763d90c59e90
Andrey Quagliato <andrey.quagliato@voalle.com.br>
2022-04-22 15:14:02 -0300

    english version

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -5,38 +5,38 @@ describe('Cadastro válido', ()=>{
         cy.get('a[href="/deliver"]').click()
         cy.get('form h1').should('have.text', 'Cadastre-se para  fazer entregas')
 
-        var entregador = {
-            nome: 'Andrey Quagliato',
+        var deliver = {
+            name: 'Andrey Quagliato',
             cpf: '66822086005',
             email: 'helloworld@gmail.com',
             whatsapp: '81938853874',
-            endereco: {
-                cep: '15991512',
-                rua: 'Avenida José Schimidt',
-                numero: '100',
-                complemento: 'Casa',
-                cidade: 'Nova Cidade',
-                estado: 'Matão/SP'
+            address: {
+                postalcode: '15991512',
+                street: 'Avenida José Schimidt',
+                number: '100',
+                details: 'Casa',
+                district: 'Nova Cidade',
+                city_uf: 'Matão/SP'
             },
-            metodo_entrega: 'Moto',
+            deliver_method: 'Moto',
             cnh: 'cnh-digital.jpg'
         }
 
-        cy.get('input[name="name"]').type(entregador.nome)
-        cy.get('input[name="cpf"]').type(entregador.cpf)
-        cy.get('input[name="email"]').type(entregador.email)
-        cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
+        cy.get('input[name="name"]').type(deliver.name)
+        cy.get('input[name="cpf"]').type(deliver.cpf)
+        cy.get('input[name="email"]').type(deliver.email)
+        cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
 
-        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
+        cy.get('input[name="postalcode"]').type(deliver.address.postalcode)
         cy.get('.field input[type="button"]').should('have.value', 'Buscar CEP').click()
 
-        cy.get('.field input[name="address"]').should('have.value', entregador.endereco.rua)
-        cy.get('.field input[name="address-number"]').type(entregador.endereco.numero)
-        cy.get('.field input[name="address-details"]').type(entregador.endereco.complemento)
-        cy.get('.field input[name="district"]').should('have.value', entregador.endereco.cidade)
-        cy.get('.field input[name="city-uf"]').should('have.value', entregador.endereco.estado)
+        cy.get('.field input[name="address"]').should('have.value', deliver.address.street)
+        cy.get('.field input[name="address-number"]').type(deliver.address.number)
+        cy.get('.field input[name="address-details"]').type(deliver.address.details)
+        cy.get('.field input[name="district"]').should('have.value', deliver.address.district)
+        cy.get('.field input[name="city-uf"]').should('have.value', deliver.address.city_uf)
 
-        cy.contains('.delivery-method li', entregador.metodo_entrega).click()
+        cy.contains('.delivery-method li', deliver.deliver_method).click()
         cy.get('input[accept^="image"]').selectFile('cypress/fixtures/images/cnh-digital.jpg', {force: true})
 
         cy.get('form button[type="submit"]').click()
@@ -44,51 +44,5 @@ describe('Cadastro válido', ()=>{
         const expectMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
 
         cy.get('#swal2-html-container').should('have.text', expectMessage)
-    })
-})
-
-describe('Cadastro CPF inválido', ()=>{
-    it('Usuário não deve conseguir se cadastrar com CPF inválido', ()=>{
-        cy.visit('https://buger-eats.vercel.app')
-        cy.get('a[href="/deliver"]').click()
-        cy.get('form h1').should('have.text', 'Cadastre-se para  fazer entregas')
-
-        var entregador = {
-            nome: 'Andrey Quagliato',
-            cpf: '668220860AA',
-            email: 'helloworld@gmail.com',
-            whatsapp: '81938853874',
-            endereco: {
-                cep: '15991512',
-                rua: 'Avenida José Schimidt',
-                numero: '100',
-                complemento: 'Casa',
-                cidade: 'Nova Cidade',
-                estado: 'Matão/SP'
-            },
-            metodo_entrega: 'Moto',
-            cnh: 'cnh-digital.jpg'
-        }
-
-        cy.get('input[name="name"]').type(entregador.nome)
-        cy.get('input[name="cpf"]').type(entregador.cpf)
-        cy.get('input[name="email"]').type(entregador.email)
-        cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
-
-        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
-        cy.get('.field input[type="button"]').should('have.value', 'Buscar CEP').click()
-
-        cy.get('.field input[name="address"]').should('have.value', entregador.endereco.rua)
-        cy.get('.field input[name="address-number"]').type(entregador.endereco.numero)
-        cy.get('.field input[name="address-details"]').type(entregador.endereco.complemento)
-        cy.get('.field input[name="district"]').should('have.value', entregador.endereco.cidade)
-        cy.get('.field input[name="city-uf"]').should('have.value', entregador.endereco.estado)
-
-        cy.contains('.delivery-method li', entregador.metodo_entrega).click()
-        cy.get('input[accept^="image"]').selectFile('cypress/fixtures/images/cnh-digital.jpg', {force: true})
-
-        cy.get('form button[type="submit"]').click()
-
-        cy.get('.alert-error').should('have.text', 'Oops! CPF inválido')
     })
 })
