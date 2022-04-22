c295317dfab2319bd186f5606e980b872c5f02bc
Andrey Quagliato <andrey.quagliato@voalle.com.br>
2022-04-22 17:51:55 -0300

    pageObjects

---
+++ cypress/pages/signUpPage.js
@@ -0,0 +1,45 @@
+
+class SignupPage {
+    go() {
+        cy.visit('https://buger-eats.vercel.app')
+        cy.get('a[href="/deliver"]').click()
+        cy.get('form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+
+    }
+
+    fillForm(deliver) {
+        cy.get('input[name="name"]').type(deliver.name)
+        cy.get('input[name="cpf"]').type(deliver.cpf)
+        cy.get('input[name="email"]').type(deliver.email)
+        cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
+
+        cy.get('input[name="postalcode"]').type(deliver.address.postalcode)
+        cy.get('.field input[type="button"]').should('have.value', 'Buscar CEP').click()
+
+        cy.get('.field input[name="address"]').should('have.value', deliver.address.street)
+        cy.get('.field input[name="address-number"]').type(deliver.address.number)
+        cy.get('.field input[name="address-details"]').type(deliver.address.details)
+        cy.get('.field input[name="district"]').should('have.value', deliver.address.district)
+        cy.get('.field input[name="city-uf"]').should('have.value', deliver.address.city_uf)
+
+        cy.contains('.delivery-method li', deliver.deliver_method).click()
+        cy.get('input[accept^="image"]').selectFile('cypress/fixtures/images/cnh-digital.jpg', {force: true})
+
+    }
+
+    submit() {
+        cy.get('form button[type="submit"]').click()
+
+    }
+
+    modalContentShouldBe(expectMessage) {
+        cy.get('#swal2-html-container').should('have.text', expectMessage)
+    }
+
+    alertMessageShouldBe(expectMessage) {
+        cy.get('.alert-error').should('have.text', expectMessage)
+
+    }
+}
+
+export default SignupPage;
