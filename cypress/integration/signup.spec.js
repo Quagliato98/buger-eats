eab5a36627f2a5a9776ffbef325c6a80ea80f327
Andrey Quagliato <andrey.quagliato@voalle.com.br>
2022-04-26 16:44:12 -0300

    add new test case

---
+++ cypress/integration/signup.spec.js
@@ -0,0 +1,49 @@
+import signup from '../pages/signUpPage'
+import signupFactory from '../factories/signupFactory'
+
+describe('Signup', ()=>{
+
+/*     beforeEach(function() {
+        cy.fixture('deliver').then((d)=> {
+            this.deliver = d
+        })
+    }) */
+
+    it('User should be deliver', function() {
+
+        var deliver = signupFactory.deliver()
+
+        signup.go()
+        signup.fillForm(deliver)
+        signup.submit()
+
+        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
+        signup.modalContentShouldBe(expectedMessage)
+    })
+    
+    it('User cant register with invalid CPF ', function() {
+
+        var deliver = signupFactory.deliver()
+
+        deliver.cpf = '668220860sd'
+
+        signup.go()
+        signup.fillForm(deliver)
+        signup.submit()
+
+        signup.alertMessageShouldBe('Oops! CPF inválido')
+    })
+
+    it('User cant register with invalid Email', function() {
+
+        var deliver = signupFactory.deliver()
+
+        deliver.email = 'helloworld.com'
+
+        signup.go()
+        signup.fillForm(deliver)
+        signup.submit()
+
+        signup.alertMessageShouldBe('Oops! Email com formato inválido.')
+    })
+})
