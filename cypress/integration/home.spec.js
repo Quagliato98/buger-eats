0be69c746c5a3695d49c0a0cf387fb661e75b35c
Andrey Quagliato <andrey.quagliato@voalle.com.br>
2022-04-26 16:36:07 -0300

    changed url

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -2,7 +2,7 @@
 
 describe ('home page', ()=>{
     it('app deve estar online', ()=>{
-        cy.visit('https://buger-eats.vercel.app')
+        cy.visit('https://buger-eats-qa.vercel.app')
         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
     })
 })
